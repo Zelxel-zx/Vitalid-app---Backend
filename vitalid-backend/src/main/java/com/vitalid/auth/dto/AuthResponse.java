5be1eb3108dca92353848d597eb6beb2c5db382b
Zelxel-zx <z.bravo016@gmail.com>
2026-05-05 16:45:44 -0500

    Update AuthResponse.java

--- vitalid-backend/src/main/java/com/vitalid/auth/dto/AuthResponse.java
+++ vitalid-backend/src/main/java/com/vitalid/auth/dto/AuthResponse.java
@@ -4,13 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+/**
+ * DTO para response de autenticación
+ * 
+ * TODO: Implement AuthResponse with:
+ * - id (user ID)
+ * - name
+ * - email
+ * - type (PATIENT, DOCTOR)
+ * - token (JWT)
+ * - message
+ */
 public record AuthResponse (
 
+    // TODO: Add properties
     Long id,
     String name,
-    String email,   
+    String email,
     UserType type,
-){}
+    String token,
+    String message
+) {}

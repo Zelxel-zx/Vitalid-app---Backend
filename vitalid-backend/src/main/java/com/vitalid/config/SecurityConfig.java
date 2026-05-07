58f9b4910e6e7ae281b6f4b78a64ac080881d56a
Zelxel-zx <z.bravo016@gmail.com>
2026-05-06 22:28:13 -0500

    feat: Password Encoder en securityconfig

--- vitalid-backend/src/main/java/com/vitalid/config/SecurityConfig.java
+++ vitalid-backend/src/main/java/com/vitalid/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.vitalid.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 
@@ -31,6 +33,11 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
